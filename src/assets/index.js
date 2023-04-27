1646ac88552d315cbd6e9da007bbd8bf69a033b0
Rishi Singh <rs3949427@gmail.com>
2023-04-27 21:56:23 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,82 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import react from './react.png';
+import hacktoberfest from './hacktoberfest.png';
+import java from './java.png';
+import cpp from './cpp.png';
+import tindog from './tindog.png';
+import weatherapp from './weatherapp.jpg';
+import sorting from './sorting.png';
+import chatbot from './chatbot.jpg';
+import netflix from './netflix.png';
+import demo from './demo.png';
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  demo,
+  tindog,
+  netflix,
+  chatbot,
+  weatherapp,
+  sorting,
+  backend,
+  creator,
+  mobile,
+  web,
+  hacktoberfest,
+  github,
+  java,
+  cpp,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  react,
+  tripguide,
+};
